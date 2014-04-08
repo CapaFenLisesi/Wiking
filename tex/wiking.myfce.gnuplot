d998c8e6d989b5c705a9b8b9c670256a888a4222
Jaroslav Fait <jardak.fait@gmail.com>
2014-04-08 18:42:20 +0200

    repository structure building
    from local directory

---
+++ tex/wiking.myfce.gnuplot
@@ -0,0 +1,2 @@
+set table "wiking.myfce.table"; set format "%.5f"
+set samples 2000; plot [x=-1.2:1.2] x*x*x
---
+++ wiking_delete.cmd
@@ -0,0 +1,18 @@
+ECHO "SMAZE SOUBORY S NASLEDUJICI PRIPONOU:"
+ECHO ""
+DEL /F /S *.bak
+DEL /F /S *.aux
+DEL /F /S *.mtc*
+DEL /F /S *.djs
+DEL /F /S *.out
+DEL /F /S *.toc
+DEL /F /S main*.xml
+DEL /F /S *.tips
+DEL /F /S *.log
+DEL /F /S *.upa
+DEL /F /S *.maf
+DEL /F /S *.bcf
+DEL /F /S *.atfi
+DEL /F /S /A H Thumbs.db
+DEL /F /S /A H .picasa.ini
+PAUSE
